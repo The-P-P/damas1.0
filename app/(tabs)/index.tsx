833f2f9233020825e5a5fda8f1c42d8075ddf4b8
Pedro Amorim <pedropauloamorim.neves@gmail.com>
2024-11-06 14:41:55 -0300

    commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image, Button} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
+ 
 
 type RootStackParamList = {
   Login: undefined;
